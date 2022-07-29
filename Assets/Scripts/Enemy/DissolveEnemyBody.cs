2e269de052aa647a64a432d54a9ca9406ca18204
ninjarlz <michal.kusmidrowicz@gmail.com>
2022-07-29 21:02:47 +0200

    general-code-refactor DissolbeEnemyBody refactor

--- Assets/Scripts/Enemy/DissolveEnemyBody.cs
+++ Assets/Scripts/Enemy/DissolveEnemyBody.cs
@@ -4,19 +4,18 @@ namespace Enemy
 {
     public class DissolveEnemyBody : MonoBehaviour
     {
+        private const string DissolveAmountShaderProp = "_DissolveAmount";
+        private static readonly int DissolveAmountID = Shader.PropertyToID(DissolveAmountShaderProp);
 
-        private const string DISSOLVE_AMOUNT_SHADER_PROP = "_DissolveAmount";
-        private Material material;
+        private Material _material;
         private EnemyControllerServer _enemyControllerServer;
-        private float _dissolveAmount = 0f;
+        private float _dissolveAmount;
         public bool Enabled { get; set; }
-        public float DissolveAmount { get => _dissolveAmount; }
-
+     
 
         private void Init()
         {
-            material = GetComponent<Renderer>().material;
-            Enabled = false;
+            _material = GetComponent<Renderer>().material;
             _enemyControllerServer = GetComponentInParent<EnemyControllerServer>();
         }
 
@@ -36,7 +35,7 @@ namespace Enemy
         private void Dissolve()
         {
             _dissolveAmount += Time.deltaTime / 3;
-            material.SetFloat(DISSOLVE_AMOUNT_SHADER_PROP, DissolveAmount);
+            _material.SetFloat(DissolveAmountID, _dissolveAmount);
             if (_dissolveAmount >= 1)
             {
                 Destroy(_enemyControllerServer.gameObject);
